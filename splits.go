5dd6857b471a4c2f06ae6eafe6550fb258c329e8
Kashif Ali <kashif.ali@realogy.com>
2021-10-19 22:08:44 +0100

    fixing splits model

--- splits.go
+++ splits.go
@@ -2,8 +2,8 @@ package strava
 
 type Split struct {
 	Distance            float64 `json:"distance"`
-	ElapsedTime         int     `json:"elapsed_time"`
+	ElapsedTime         float64 `json:"elapsed_time"`
 	ElevationDifference float64 `json:"elevation_difference"`
-	MovingTime          int     `json:"moving_time"`
+	MovingTime          float64 `json:"moving_time"`
 	Split               int     `json:"split"`
 }
